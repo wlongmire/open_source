b4324cf7da0d8841e33da9e459888189d8415c31
Warren Longmire <warren.longmire@lambdaschool.com>
2021-07-12 21:52:06 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+import { Route } from 'react-router-dom';
+import data from './data';
 import './App.css';
 
+class Source {
+  constructor(title, url, id) {
+    this.title = title;
+    this.baseUrl = url;
+    this.id = id;
+  }
+
+  getURL = () => {
+    return (`https://www.google.com/search?q=${this.getShortCode()}`);
+  }
+
+  getRoute = () => {
+    return (`/footnote/${this.id}`);
+  }
+
+  getShortCode = () => {
+    return (this.title.toLowerCase().trim().replace(" ", "-"));
+  }
+}
+
 function App() {
+
+  const sources = data.map(src => new Source(src.title, src.url, src.id));
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>Open Source Resources</h2>
       </header>
+      {
+        sources.map(src => <Route key={src.id} path={src.getRoute()} render={() => (window.location.href = `${src.getURL()}`)} />)
+      }
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,217 @@
+export default [
+  {
+    id: 1,
+    title: "baldwin",
+    url: "https://www.google.com/search?q=baldwin"
+  },
+  {
+    id: 2,
+    title: "Strawberry Mansion",
+    url: "https://www.google.com/search?q=baldwin"
+  },
+  {
+    id: 3,
+    title: "The Invariant Be",
+    url: "https://www.google.com/search?q=baldwin"
+  },
+  {
+    id: 4,
+    title: "baldwin",
+    url: "https://www.google.com/search?q=baldwin"
+  },
+  {
+    id: 5,
+    title: "Strawberry Mansion",
+    url: "https://www.google.com/search?q=baldwin"
+  },
+  {
+    id: 6,
+    title: "The Invariant Be",
+    url: "https://www.google.com/search?q=baldwin"
+  },
+  {
+    id: 7,
+    title: "baldwin",
+    url: "https://www.google.com/search?q=baldwin"
+  },
+  {
+    id: 8,
+    title: "Strawberry Mansion",
+    url: "https://www.google.com/search?q=baldwin"
+  },
+  {
+    id: 9,
+    title: "The Invariant Be",
+    url: "https://www.google.com/search?q=baldwin"
+  },
+  {
+    id: 10,
+    title: "baldwin",
+    url: "https://www.google.com/search?q=baldwin"
+  },
+  {
+    id: 12,
+    title: "Strawberry Mansion",
+    url: "https://www.google.com/search?q=baldwin"
+  },
+  {
+    id: 13,
+    title: "The Invariant Be",
+    url: "https://www.google.com/search?q=baldwin"
+  },
+  {
+    id: 14,
+    title: "baldwin",
+    url: "https://www.google.com/search?q=baldwin"
+  },
+  {
+    id: 15,
+    title: "Strawberry Mansion",
+    url: "https://www.google.com/search?q=baldwin"
+  },
+  {
+    id: 16,
+    title: "The Invariant Be",
+    url: "https://www.google.com/search?q=baldwin"
+  },
+  {
+    id: 17,
+    title: "baldwin",
+    url: "https://www.google.com/search?q=baldwin"
+  },
+  {
+    id: 18,
+    title: "Strawberry Mansion",
+    url: "https://www.google.com/search?q=baldwin"
+  },
+  {
+    id: 19,
+    title: "The Invariant Be",
+    url: "https://www.google.com/search?q=baldwin"
+  },
+  {
+    id: 20,
+    title: "The Invariant Be",
+    url: "https://www.google.com/search?q=baldwin"
+  },
+  {
+    id: 21,
+    title: "baldwin",
+    url: "https://www.google.com/search?q=baldwin"
+  },
+  {
+    id: 22,
+    title: "Strawberry Mansion",
+    url: "https://www.google.com/search?q=baldwin"
+  },
+  {
+    id: 23,
+    title: "The Invariant Be",
+    url: "https://www.google.com/search?q=baldwin"
+  },
+  {
+    id: 24,
+    title: "baldwin",
+    url: "https://www.google.com/search?q=baldwin"
+  },
+  {
+    id: 25,
+    title: "Strawberry Mansion",
+    url: "https://www.google.com/search?q=baldwin"
+  },
+  {
+    id: 26,
+    title: "The Invariant Be",
+    url: "https://www.google.com/search?q=baldwin"
+  },
+  {
+    id: 27,
+    title: "baldwin",
+    url: "https://www.google.com/search?q=baldwin"
+  },
+  {
+    id: 28,
+    title: "Strawberry Mansion",
+    url: "https://www.google.com/search?q=baldwin"
+  },
+  {
+    id: 29,
+    title: "The Invariant Be",
+    url: "https://www.google.com/search?q=baldwin"
+  },
+  {
+    id: 30,
+    title: "baldwin",
+    url: "https://www.google.com/search?q=baldwin"
+  },
+  {
+    id: 32,
+    title: "Strawberry Mansion",
+    url: "https://www.google.com/search?q=baldwin"
+  },
+  {
+    id: 33,
+    title: "The Invariant Be",
+    url: "https://www.google.com/search?q=baldwin"
+  },
+  {
+    id: 34,
+    title: "baldwin",
+    url: "https://www.google.com/search?q=baldwin"
+  },
+  {
+    id: 35,
+    title: "Strawberry Mansion",
+    url: "https://www.google.com/search?q=baldwin"
+  },
+  {
+    id: 36,
+    title: "The Invariant Be",
+    url: "https://www.google.com/search?q=baldwin"
+  },
+  {
+    id: 37,
+    title: "baldwin",
+    url: "https://www.google.com/search?q=baldwin"
+  },
+  {
+    id: 38,
+    title: "Strawberry Mansion",
+    url: "https://www.google.com/search?q=baldwin"
+  },
+  {
+    id: 39,
+    title: "The Invariant Be",
+    url: "https://www.google.com/search?q=baldwin"
+  },
+  {
+    id: 40,
+    title: "The Invariant Be",
+    url: "https://www.google.com/search?q=baldwin"
+  },
+  {
+    id: 41,
+    title: "baldwin",
+    url: "https://www.google.com/search?q=baldwin"
+  },
+  {
+    id: 42,
+    title: "Strawberry Mansion",
+    url: "https://www.google.com/search?q=baldwin"
+  },
+  {
+    id: 43,
+    title: "The Invariant Be",
+    url: "https://www.google.com/search?q=baldwin"
+  },
+  {
+    id: 44,
+    title: "baldwin",
+    url: "https://www.google.com/search?q=baldwin"
+  },
+  {
+    id: 45,
+    title: "Strawberry Mansion",
+    url: "https://www.google.com/search?q=baldwin"
+  }
+];
