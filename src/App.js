c1332845cd191056c59fba4ef305eb40e1a88a82
Warren Longmire <warren.longmire@lambdaschool.com>
2021-08-07 09:10:18 -0400

    moved FrameComponent.

--- src/App.js
+++ src/App.js
@@ -8,19 +8,6 @@ import data from './data';
 import './App.css';
 
 
-const FrameComponent = () => {
-  return (<div class="page">
-    <Iframe url="http://www.youtube.com/embed/xDMP3i36naA"
-      width="450px"
-      height="1000px"
-      className="frame" />
-    <Iframe url="http://webcache.googleusercontent.com/search?q=cache:https://www.phillymag.com/news/2021/07/10/philly-inferiority-complex/"
-      width="450px"
-      height="1000px"
-      className="frame" />
-  </div>);
-}
-
 function App() {
 
   const sources = data.map(src => {
