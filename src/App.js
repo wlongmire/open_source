6f5f9d364ae3530b8beff25464971dbed4f3c4c5
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-08-30 22:15:28 -0400

    adds media query

--- src/App.js
+++ src/App.js
@@ -23,13 +23,12 @@ function App() {
         <img width="100px" height="100px" src="./assets/open.png"/>
       </Header>
       <Mosaic>
+        <SmallItem>
+        <iframe className="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d24454.867806501945!2d-75.19231844229078!3d39.98926793267725!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c6c792ace88039%3A0x8b90a2aa5e25a819!2sStrawberry%20Mansion%2C%20Philadelphia%2C%20PA!5e0!3m2!1sen!2sus!4v1630374856051!5m2!1sen!2sus" width="100%" height="100%" allowfullscreen="" loading="lazy"></iframe>
+        </SmallItem>
         <LargeItem>
           <ReactPlayer playing={playing} pip={false} controls={false} light={false} width="100%" height="100%" url="https://www.youtube.com/watch?v=SGGW51EGUrA"/>
         </LargeItem>
-        <SmallItem>
-          <iframe className="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d24454.867806501945!2d-75.19231844229078!3d39.98926793267725!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c6c792ace88039%3A0x8b90a2aa5e25a819!2sStrawberry%20Mansion%2C%20Philadelphia%2C%20PA!5e0!3m2!1sen!2sus!4v1630374856051!5m2!1sen!2sus" width="100%" height="100%" allowfullscreen="" loading="lazy"></iframe>
-        </SmallItem>
-        
       </Mosaic>
       <div>
         {!playing && <Modal>
@@ -78,7 +77,6 @@ const Container = styled.div`
   flex-direction: column;
   
 `
-
 const Header = styled.header`
   box-sizing: border-box;
   background-color: black;
@@ -99,13 +97,18 @@ const Mosaic = styled.div`
   display:grid;
   box-sizing: border-box;
   height:100%;
-  
-  grid-template-rows: repeat(6, 1fr);
-  grid-template-columns: repeat(3, 1fr);
+
   padding: 0.2em;
   grid-gap: 0.2em;
-`
 
+  grid-template-rows: repeat(4, 1fr);
+  grid-template-columns: repeat(3, 1fr);
+  
+  @media (max-width: 768px) {
+    grid-template-rows: repeat(6, 1fr);
+    grid-template-columns: repeat(3, 1fr);
+  }
+`
 const Item = styled.div`
   background-color: ${props => props.bgColor || "grey"};
   color: ${props => props.color || "white"};
@@ -116,13 +119,22 @@ const Item = styled.div`
 
 const SmallItem = styled(Item)`
   grid-row: span 4;
-  grid-column: span 4;
+  grid-column: span 1;
+
+  @media (max-width: 768px) {
+    grid-row: 3 / 7;
+    grid-column: span 4;
+  }
 `
 const LargeItem = styled(Item)`
-  grid-row: span 2;
-  grid-column: span 4;
-`
+  grid-row: span 4;
+  grid-column: span 2;
 
+  @media (max-width: 768px) {
+    grid-row: 1 / 3;
+    grid-column: span 4;
+  }
+`
 
 // <div className="box red o75">
 //             Top frame
