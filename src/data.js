9f48802176bf3a312253c147885f9942d04dc29d
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-09-01 23:35:40 -0400

    more data changes.

--- src/data.js
+++ src/data.js
@@ -49,7 +49,6 @@ const data = [
       modules:[
         {
           type:VIDEO_TYPE,
-          muted: false,
           id: "SwaUa7Umu-A",
           row_range: {start: 1, end:6},
           col_range: {start: 1, end:10},
