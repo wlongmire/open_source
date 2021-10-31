46d4b14c0c0115f79b4215843fddd7f93fc9d2c6
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-10-31 12:42:04 -0400

    adds preorder link

--- src/App.js
+++ src/App.js
@@ -70,6 +70,9 @@ function App() {
     <Route exact path= "/" render={()=>{
       window.location.href = 'https://linktr.ee/alongmirewriter';
     }}/>
+    <Route exact path= "/opensource" render={()=>{
+      window.location.href = 'https://www.radiatorpress.com/product/open-source-by-warren-c-longmire';
+    }}/>
     <Route exact path= "/publications">
       <h2>Publications</h2>
     </Route>
